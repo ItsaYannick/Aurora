68b9a669135c67a4ff424dea665f8bc98775f4df
ItsaYannick <jaxterlp@gmail.com>
2025-09-11 22:45:27 +0200

    Fixed import responses

--- src/Aurora-0.1.0/Aurora.py
+++ src/Aurora-0.1.0/Aurora.py
@@ -15,14 +15,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-import sys
-import os
-sys.path.append("/usr/lib/aurora")
-import responses
-
 import subprocess
 import random
 from rich import print
+import responses
 import os
 import datetime
 import time
